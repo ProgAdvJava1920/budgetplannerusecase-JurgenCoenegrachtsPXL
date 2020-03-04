7f5f5489cff8ef289d48eea4d9df069e66554c13
Jurgen Coenegrachts <jurgen.coenegrachts@student.pxl.be>
2020-03-04 12:14:11 +0100

    parser af

--- src/main/java/be/pxl/student/util/BudgetPlannerMapper.java
+++ src/main/java/be/pxl/student/util/BudgetPlannerMapper.java
@@ -2,44 +2,73 @@ package be.pxl.student.util;
 
 import be.pxl.student.entity.Account;
 import be.pxl.student.entity.Payment;
+import jdk.jshell.spi.ExecutionControl;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class BudgetPlannerMapper {
 
     // TODO: move to property file
     public static final String DATE_PATTERN = "EEE MMM d HH:mm:ss z yyyy";
     public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(DATE_PATTERN, Locale.US);
+    public static final int CSV_ITEM_COUNT = 7;
+
+    private Map<String, Account> accountMap = new HashMap<>();
 
     public List<Account> mapAccounts(List<String> accountLines) {
         //       v---- cursor here, alt+enter, 2nd option (convert...)
         //return accountLines.stream().map(this::mapDataLineToAccount).collect(Collectors.toList());
 
-        List<Account> list = new ArrayList<>();
+        //List<Account> list = new ArrayList<>();
         for (String accountLine : accountLines) {
-            Account account = mapDataLineToAccount(accountLine);
+            Account account = null;
 
-            if (!list.contains(account)) {
-                list.add(account);
+            // skip bad lines, continue parsing
+            try {
+                // parse account (and payment) from line
+                account = mapDataLineToAccount(accountLine);
+
+                // if account isn't in the map yet, put it in
+                accountMap.putIfAbsent(account.getIBAN(), account);
+
+
+                /*
+                if (!list.contains(account)) {
+                    list.add(account);
+                } else {
+                    list.get(list.indexOf(account)).getPayments().add(account.getPayments().get(0));
+                }
+                */
+            } catch (ParseException | BudgetPlannerException e) {
+                System.err.println(String.format("Could not parse line [%s]", accountLine));
             }
         }
 
-        return list;
+        //return list;
+        return new ArrayList<>(accountMap.values());
     }
 
-    public Account mapDataLineToAccount(String line) {
+    public Account mapDataLineToAccount(String line) throws ParseException, BudgetPlannerException {
         String[] items = line.split(",");
+
+        if (items.length != 7) {
+            throw new BudgetPlannerException(String.format("Expected %d fields in line, but found %d", CSV_ITEM_COUNT, items.length));
+        }
 
         String name = items[0];
         String iban = items[1];
-        Account newAccount = new Account(name, iban);
 
-        return newAccount;
+        // get existing account from map, if it doesn't doesn't exist in the map create a new account
+        Account account = accountMap.getOrDefault(iban, new Account(name, iban));
+
+        //Account account = new Account(name, iban);
+
+        Payment payment = mapItemsToPayment(items);
+        account.getPayments().add(payment);
+
+        return account;
     }
 
     public Date convertToDate(String dateString) throws ParseException {
@@ -48,5 +77,15 @@ public class BudgetPlannerMapper {
 
     public String convertDateToString(Date date) {
         return DATE_FORMAT.format(date);
+    }
+
+    public Payment mapItemsToPayment(String[] items) throws ParseException {
+        return new Payment(
+                items[2],                       // IBAN
+                convertToDate(items[3]),        // transaction date
+                Float.parseFloat(items[4]),     // amount
+                items[5],                       // currency
+                items[6]                        // comment
+        );
     }
 }
